18c62615f8c19c6d27308c251878aac601d00d41
Refik Fatih Ustok <fatih.ustok@gmail.com>
2016-05-20 18:46:07 +1200

    Initial Commit

---
+++ Virtual_Tourist_FatihUstokTests/Virtual_Tourist_FatihUstokTests.swift
@@ -0,0 +1,36 @@
+//
+//  Virtual_Tourist_FatihUstokTests.swift
+//  Virtual_Tourist_FatihUstokTests
+//
+//  Created by Refik Fatih Ustok on 20/05/16.
+//  Copyright © 2016 Refik Fatih Ustok. All rights reserved.
+//
+
+import XCTest
+@testable import Virtual_Tourist_FatihUstok
+
+class Virtual_Tourist_FatihUstokTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
